ea9d3214b618fd30f08e4003f2f1fc9744d09a07
Ellery Ames <amesellery@gmail.com>
2022-06-02 08:58:41 -0700

    Remove temporary sleep.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -29,10 +29,6 @@ void MessageQueue<T>::send(T&& msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 
-    // simulate some work
-    // TODO: Remove me in final.
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
     // perform vector modification under the lock
     std::lock_guard<std::mutex> uLock(_mutex);
 
